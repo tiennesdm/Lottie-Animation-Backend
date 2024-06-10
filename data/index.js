790e91b4e54a9222aa2160cfdc1f9f6ae950081f
shubham mehta <shubham.mehta.c@stc.com.kw>
2024-06-10 23:40:55 +0300

    Added File Data

---
+++ data/index.js
@@ -0,0 +1,15 @@
+module.exports.tasks = [
+    { id: "1", name: "Work", completed: false, userId: "3" },
+    { id: "2", name: "Eat", completed: true, userId: "1" },
+    { id: "3", name: "Shopping", completed: true, userId: "4" },
+    { id: "4", name: "Gym", completed: true, userId: "4" },
+    { id: "5", name: "Eat", completed: true, userId: "3" },
+    { id: "6", name: "Shopping", completed: true, userId: "2" },
+    { id: "7", name: "Gym", completed: true, userId: "1" },
+];
+module.exports.users = [
+    { id: "1", name: "sachin", email: "sachin@gmail.com" },
+    { id: "2", name: "virat", email: "virat@gmail.com" },
+    { id: "3", name: "salman", email: "salman@gmail.com" },
+    { id: "4", name: "ak", email: "ak@gmail.com" },
+];
